4920424635d628de036ae2c9d33536f94e4c7ead
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-12-05 00:10:39 +0000

    Neater solution to day 3 part 1

--- day3/Main.elm
+++ day3/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import Dict exposing (Dict)
 
 
 main : Program Never Model Msg
@@ -67,7 +68,7 @@ update msg model =
                             , output =
                                 Ok
                                     { part1 = solvePart1 input
-                                    , part2 = 0
+                                    , part2 = 0 --solvePart2 input
                                     }
                             }
             in
@@ -116,15 +117,6 @@ type Direction
     | Down
 
 
-solvePart1 : Int -> Int
-solvePart1 input =
-    let
-        ( x, y ) =
-            move input 1 3 Up 0 0
-    in
-        (Basics.abs x) + (Basics.abs y)
-
-
 
 {-
    17  16  15  14  13
@@ -136,10 +128,76 @@ solvePart1 input =
 -}
 
 
+solvePart1 : Int -> Int
+solvePart1 input =
+    let
+        ( x, y ) =
+            move input 1 1 Right 0 0
+    in
+        (Basics.abs x) + (Basics.abs y)
+
+
 move : Int -> Int -> Int -> Direction -> Int -> Int -> ( Int, Int )
 move target current sideLength direction x y =
     if target == current then
         ( x, y )
+    else
+        case direction of
+            Up ->
+                if y == sideLength // 2 then
+                    move target (current + 1) sideLength Left (x - 1) y
+                else
+                    move target (current + 1) sideLength Up x (y + 1)
+
+            Left ->
+                if x == (negate sideLength) // 2 then
+                    move target (current + 1) sideLength Down x (y - 1)
+                else
+                    move target (current + 1) sideLength Left (x - 1) y
+
+            Down ->
+                if y == (negate sideLength) // 2 then
+                    move target (current + 1) sideLength Right (x + 1) y
+                else
+                    move target (current + 1) sideLength Down x (y - 1)
+
+            Right ->
+                if x == sideLength // 2 then
+                    move target (current + 1) (sideLength + 2) Up (x + 1) y
+                else
+                    move target (current + 1) sideLength Right (x + 1) y
+
+
+
+{-
+   147  142  133  122   59
+   304    5    4    2   57
+   330   10    1    1   54
+   351   11   23   25   26
+   362  747  806--->   ...
+
+-}
+
+
+solvePart2 : Int -> Int
+solvePart2 input =
+    let
+        grid =
+            Dict.insert ( 0, 0 ) 1 Dict.empty
+    in
+        test input 1 3 Up 0 0 grid
+
+
+type alias Grid =
+    Dict ( Int, Int ) Int
+
+
+test : Int -> Int -> Int -> Direction -> Int -> Int -> Grid -> Int
+test target current sideLength direction x y grid =
+    if Dict.size grid > 100 then
+        current
+    else if current > target then
+        current
     else
         let
             distanceToTravel =
@@ -148,30 +206,102 @@ move target current sideLength direction x y =
             nextNumber =
                 current + distanceToTravel
 
-            _ =
-                Debug.log ""
-                    { target = target
-                    , current = current
-                    , sideLength = sideLength
-                    , direction = direction
-                    , x = x
-                    , y = y
-                    , distanceToTravel = distanceToTravel
-                    , nextNumber = nextNumber
-                    }
+            -- _ =
+            --     Debug.log "grid" grid
+            -- { target = target
+            -- , current = current
+            -- , sideLength = sideLength
+            -- , direction = direction
+            -- , x = x
+            -- , y = y
+            -- , distanceToTravel = distanceToTravel
+            -- , nextNumber = nextNumber
+            -- }
         in
             case direction of
                 Up ->
-                    move target nextNumber sideLength Left (x + 1) (y - 1 + distanceToTravel)
+                    let
+                        nextX =
+                            x + 1
+
+                        nextY =
+                            y - 1 + distanceToTravel
+
+                        points =
+                            Debug.log (toString ( y, nextY )) <|
+                                List.map (\yi -> ( nextX, yi )) (List.range y nextY)
+
+                        newGrid =
+                            List.foldl insertIntoGrid grid points
+                    in
+                        test target nextNumber sideLength Left nextX nextY newGrid
 
                 Left ->
-                    move target nextNumber sideLength Down (x - distanceToTravel) y
+                    let
+                        nextX =
+                            x - distanceToTravel
+
+                        points =
+                            List.range nextX x
+                                |> List.reverse
+                                |> List.map (\xi -> ( xi, y ))
+
+                        newGrid =
+                            List.foldl insertIntoGrid grid points
+                    in
+                        test target nextNumber sideLength Down nextX y newGrid
 
                 Down ->
-                    move target nextNumber sideLength Right x (y - distanceToTravel)
+                    let
+                        nextY =
+                            y - distanceToTravel
+
+                        points =
+                            List.range nextY y
+                                |> List.reverse
+                                |> List.map (\yi -> ( x, yi ))
+
+                        newGrid =
+                            List.foldl insertIntoGrid grid points
+                    in
+                        test target nextNumber sideLength Right x nextY newGrid
 
                 Right ->
-                    move target nextNumber (sideLength + 2) Up (x + distanceToTravel) y
+                    let
+                        nextX =
+                            x + distanceToTravel
+
+                        points =
+                            List.map (\xi -> ( xi, y )) (List.range x nextX)
+
+                        newGrid =
+                            List.foldl insertIntoGrid grid points
+                    in
+                        test target nextNumber (sideLength + 2) Up nextX y newGrid
+
+
+insertIntoGrid : ( Int, Int ) -> Grid -> Grid
+insertIntoGrid currentPoint grid =
+    let
+        value =
+            neighbourCoords currentPoint
+                |> List.filterMap (\point -> Dict.get point grid)
+                |> List.sum
+    in
+        Dict.insert currentPoint value grid
+
+
+neighbourCoords : ( Int, Int ) -> List ( Int, Int )
+neighbourCoords ( x, y ) =
+    [ ( x - 1, y - 1 )
+    , ( x - 1, y )
+    , ( x - 1, y + 1 )
+    , ( x, y - 1 )
+    , ( x, y + 1 )
+    , ( x + 1, y - 1 )
+    , ( x + 1, y )
+    , ( x + 1, y + 1 )
+    ]
 
 
 view : Model -> Html Msg
